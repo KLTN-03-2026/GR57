594802c43e26c4c8da32bebbc254699b3cf811e5
huynh tan thinh <huynhtanthinh11993@gmail.com>
2026-03-29 19:00:57 +0700

    hocvienprofile

--- ums_backend/src/main/java/com/university/controller/controller/student/ChuongTrinhDaoTaoController.java
+++ ums_backend/src/main/java/com/university/controller/controller/student/ChuongTrinhDaoTaoController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.UUID;
+
 
 @RestController
 @RequestMapping("/api/student/chuong-trinh-dao-tao")
